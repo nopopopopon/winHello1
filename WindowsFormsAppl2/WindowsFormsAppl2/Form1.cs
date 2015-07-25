30501fbcd2bf147a89b62e042add8a880d6bf048
nopopopopon <mail@sweetmint.org>
2015-07-25 13:22:02 +0900

    test2

---
+++ WindowsFormsAppl2/WindowsFormsAppl2/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppl2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ipath1 = "c:\\tmp1\\無題.bmp";
+            pictureBox1.Load(ipath1);
+        }
+    }
+}
